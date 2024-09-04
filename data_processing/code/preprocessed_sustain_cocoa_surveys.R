81e043787e85b2870da20fadde2adeabdf420d27
Valentin Guye <valentin.guye@uclouvain.be>
2024-09-04 19:06:45 +0200

    significant change to how sustaincocoa survey data is matched to IC2B

--- data_processing/code/preprocessed_sustain_cocoa_surveys.R
+++ data_processing/code/preprocessed_sustain_cocoa_surveys.R
@@ -29,19 +29,19 @@ vil = read_xlsx(here("input_data", "sustain_cocoa", "surveys_civ", "Village_leve
                     col_names = TRUE) %>% 
   rename(VILLAGE_SURVEY_ID = `_index`) 
 
-surv_coop = read_xlsx(here("input_data", "sustain_cocoa", "surveys_civ", "Village_level_survey_Cote_dIvoire_-_all_versions_-_False_-_2023-12-04-14-44-13.xlsx"), 
-                 sheet = "SSI_cooperative_overview", 
-                 col_names = TRUE) %>% 
-  rename(VILLAGE_SURVEY_ID = `_parent_index`, 
-         COOP_SURVEY_ID = `_index`) %>% 
-  mutate(BUYER_TYPE = "COOPERATIVE")
+# surv_coop = read_xlsx(here("input_data", "sustain_cocoa", "surveys_civ", "Village_level_survey_Cote_dIvoire_-_all_versions_-_False_-_2023-12-04-14-44-13.xlsx"), 
+#                  sheet = "SSI_cooperative_overview", 
+#                  col_names = TRUE) %>% 
+#   rename(VILLAGE_SURVEY_ID = `_parent_index`, 
+#          COOP_SURVEY_ID = `_index`) %>% 
+#   mutate(BUYER_TYPE = "COOPERATIVE")
 
-trait = read_xlsx(here("input_data", "sustain_cocoa", "surveys_civ", "Village_level_survey_Cote_dIvoire_-_all_versions_-_False_-_2023-12-04-14-44-13.xlsx"), 
-                  sheet = "SSI_traitant_overview", 
-                  col_names = TRUE) %>% 
-  rename(VILLAGE_SURVEY_ID = `_parent_index`, 
-         TRAITANT_SURVEY_ID = `_index`) %>% 
-  mutate(BUYER_TYPE = "TRAITANT")
+# trait = read_xlsx(here("input_data", "sustain_cocoa", "surveys_civ", "Village_level_survey_Cote_dIvoire_-_all_versions_-_False_-_2023-12-04-14-44-13.xlsx"), 
+#                   sheet = "SSI_traitant_overview", 
+#                   col_names = TRUE) %>% 
+#   rename(VILLAGE_SURVEY_ID = `_parent_index`, 
+#          TRAITANT_SURVEY_ID = `_index`) %>% 
+#   mutate(BUYER_TYPE = "TRAITANT")
 
 choices = read_xlsx(here("input_data", "sustain_cocoa", "surveys_civ", "ref_survey_041223.xlsx"), 
                   sheet = "choices", 
@@ -61,120 +61,166 @@ coopbs <-
   
 
 # Departements (districts)
-departements <- s3read_using(
-  object = "cote_divoire/spatial/BOUNDARIES/DEPARTEMENT/OUT/CIV_DEPARTEMENTS.geojson",#"cote_divoire/spatial/BOUNDARIES/DEPARTEMENT/OUT/ci_departments_wgs84_level4.geojson",
-  bucket = "trase-storage",
-  FUN = read_sf,
-  #sheet = "Cacao",
-  #skip = 3,
-  opts = c("check_region" = T)
-)
+departements <- read_sf("input_data/s3/CIV_DEPARTEMENTS.geojson")
+
 departements = 
   st_transform(departements, crs = civ_crs)
 
 
 # Join with IC2B -----
 
+# Join directly with village-level survey
+
 # Join on abbreviated name to add the coordinates to the survey data.
 
 ## Prepare joining keys ----------
-
-allcoop = 
+sc_coop_names = 
   choices %>% 
   filter(list_name == "cooperative") %>% 
   select(COOP_ABRV_NAME = `label::Francais (fr)`, 
          COOP_SURVEY_KEY = name) %>% 
   filter(!COOP_SURVEY_KEY %in% c("DK", "other"))
-# allcoop %>% filter(`label::Francais (fr)` != `label::English (en)`)
-# allcoop$label %>% unique()
+# sc_coop_names %>% filter(`label::Francais (fr)` != `label::English (en)`)
+# sc_coop_names$label %>% unique()
 
-surv_coop = 
-  surv_coop %>% 
-  # merged on surv_coop key in survey to add the surv_coop name 
-  left_join(allcoop, 
-            by = join_by("ssicooperative_name" == "COOP_SURVEY_KEY")) %>% 
-  # When the key is missing, no name is matched. In this case, take the name responded in 'other'
-  mutate(COOP_ABRV_NAME = case_when(
-    is.na(COOP_ABRV_NAME) ~ ssicooperative_other, 
-    TRUE ~ COOP_ABRV_NAME
-  )) %>% 
-  filter(!is.na(COOP_ABRV_NAME)) %>% 
+
+## Prepare joining keys -----------
+# The coop IDs are in variable village_buyer_cooperative_list 
+
+# Split rows (one by village) by the number of coops they are linked with
+vil_links = 
+  vil %>% 
+  mutate(
+    village_buyer_cooperative_list = as.character(village_buyer_cooperative_list),
+    village_buyer_cooperative = str_split(village_buyer_cooperative_list, pattern = " "),
+    village_buyer_cooperative = map(village_buyer_cooperative, str_squish)
+  ) %>%
+  unnest(cols = c(village_buyer_cooperative)) %>% 
+  # vil_links$village_buyer_cooperative %>% unique()
+  
+  # convert these keys into names 
+  left_join(sc_coop_names, 
+            by = join_by("village_buyer_cooperative" == "COOP_SURVEY_KEY")) %>% 
+  # vil_links$COOP_ABRV_NAME %>% unique()
+  
+  # When the key is missing, or equal to "other", no name is matched from the list sc_coop_names.
+  # In this case, take the name responded in village_buyer_cooperative_list
+  mutate(
+    # but clean this variable before 
+    village_buyer_cooperative_other = str_squish(
+      gsub(pattern = "[(1)]|[(2)]|[(3)]|[(4)]|[(5)]|Société coopérative de Koffikro et d'Abouyo ", 
+           replacement = "", 
+           village_buyer_cooperative_other)
+      ),
+    village_buyer_cooperative_other = str_squish(
+      gsub(pattern = "[/]| et ", 
+           replacement = ";", 
+           village_buyer_cooperative_other)
+    ),
+    village_buyer_cooperative_other = gsub("Scoops rasso Scoop kany Sccni Scoop wuntaba", 
+                                           "SCOOPS RASSO; SCOOP KANY SCCNI; SCOOP WUNTABA", 
+                                           village_buyer_cooperative_other)
+  ) %>% 
+  # vil_links$village_buyer_cooperative_other %>% unique()
+  mutate(
+    COOP_ABRV_NAME = case_when(
+      is.na(COOP_ABRV_NAME) ~ village_buyer_cooperative_other, 
+      TRUE ~ COOP_ABRV_NAME
+    )
+  ) %>% 
+  # vil_links$COOP_ABRV_NAME %>% unique()
+  # some "other" values are multiple coops, so split rows again here 
+  mutate(
+    COOP_ABRV_NAME = str_split(COOP_ABRV_NAME, pattern = ";"),
+    COOP_ABRV_NAME = map(COOP_ABRV_NAME, str_squish)
+  ) %>%
+  unnest(cols = c(COOP_ABRV_NAME)) %>% 
+  # vil_links$COOP_ABRV_NAME %>% unique()
+  mutate(COOP_ABRV_NAME = if_else(COOP_ABRV_NAME %in% c("", "DK"), NA, COOP_ABRV_NAME)) %>% 
+  
+  # DO NOT remove villages selling to no coop
+  # filter(!is.na(COOP_ABRV_NAME)) %>% 
+  
   # clean these abbreviated names like they were cleaned for IC2B
-  mutate(COOP_SIMPLIF_ABRV_NAME = fn_clean_abrvname3(fn_clean_abrvname2(fn_clean_abrvname1(str_trans(str_squish(COOP_ABRV_NAME))))))
+  mutate(COOP_SIMPLIF_ABRV_NAME = fn_clean_abrvname3(fn_clean_abrvname2(fn_clean_abrvname1(str_trans(str_squish(COOP_ABRV_NAME))))), 
+         COOP_SIMPLIF_ABRV_NAME = gsub("COOPS CA ", "", COOP_SIMPLIF_ABRV_NAME))
 
-surv_coop %>% 
+  # check them 
+  (simplif_names = unique(vil_links$COOP_SIMPLIF_ABRV_NAME))
+
+vil_links %>% 
   distinct(COOP_ABRV_NAME, .keep_all = T) %>% 
   select(COOP_ABRV_NAME, COOP_SIMPLIF_ABRV_NAME) %>% 
   View()
-
-simplif_names = unique(surv_coop$COOP_SIMPLIF_ABRV_NAME)
 
 ## Prepare IC2B to join --------
 
 # limit coopbs to unique rows and buying stations which it makes sense to match. 
 coopbs_tomatch = 
   coopbs %>% 
-  # and useful rows, i.e. those that have coordinates
-  filter(!is.na(LONGITUDE)) %>% 
+  # keep only rows with coordinates (useless otherwise) 
+  # and with an abbreviated name to matched on.  
+  filter(!is.na(LONGITUDE) & !is.na(SUPPLIER_ABRVNAME)) %>% 
   filter(SIMPLIF_ABRVNAME %in% simplif_names) %>% 
   # just reproduce rounded coords
-  mutate(ROUND_LONGITUDE = round(LONGITUDE, 2), 
-         ROUND_LATITUDE = round(LATITUDE, 2)) %>% 
-  select(COOP_ID, COOP_POINT_ID, COOP_BS_ID, SIMPLIF_ABRVNAME, SUPPLIER_ABRVNAME, SUPPLIER_FULLNAME, ROUND_LATITUDE, ROUND_LONGITUDE, DISTRICT_NAME, everything())  %>% 
+  # mutate(ROUND_LONGITUDE = round(LONGITUDE, 2), 
+  #        ROUND_LATITUDE = round(LATITUDE, 2)) %>% 
+  select(COOP_ID, COOP_POINT_ID, COOP_BS_ID, SIMPLIF_ABRVNAME, SUPPLIER_ABRVNAME, SUPPLIER_FULLNAME, DISTRICT_NAME,
+         # ROUND_LATITUDE, ROUND_LONGITUDE,  
+         BS_LONGITUDE = LONGITUDE, BS_LATITUDE = LATITUDE, everything())  %>% 
   arrange(SIMPLIF_ABRVNAME)
-  
-# we want to avoid homonyms in simplif abrv names, but here it's not possible to arbitrate which is the actual surv_coop, 
-# because we have no more info and there are many homonyms in some cases (more than 4). 
-coopbs_tomatch %>% 
-  group_by(SIMPLIF_ABRVNAME) %>% 
-  mutate(N_DIFF_BS = length(unique(COOP_BS_ID))) %>% 
-  ungroup() %>%   
-  arrange(desc(N_DIFF_BS)) %>% 
-  select(N_DIFF_BS, everything()) %>% 
-  View()
 
 
 ## Join coop survey with IC2B -------
 
 #  To give IC2B attributes (at bs-level) to survey coop data 
-surv_coop_bs = 
-  surv_coop %>% 
-  # inner_join because we are not interested in keeping coops matched with nothing in IC2B here. 
-  inner_join(coopbs_tomatch, 
-            by = join_by("COOP_SIMPLIF_ABRV_NAME" == "SIMPLIF_ABRVNAME"), 
-            multiple = "all") # multiple matches are expected, coops have several buying stations in IC2B.
+vil_links_bs = 
+  vil_links %>% 
+  # left_join because we want to keep links with other buyers than coops. 
+  left_join(coopbs_tomatch, 
+             by = join_by("COOP_SIMPLIF_ABRV_NAME" == "SIMPLIF_ABRVNAME"), 
+             multiple = "all") # multiple matches are expected, coops have several buying stations in IC2B.
 
-# Options to handle multiple matches (homonyms in abbreviated names)
+# This introduces duplicates for different reasons: 
+# - an abbreviated name in survey data may match several distinct coops with the same abbreviated name 
+# - an abbreviated name in survey data may match a single coop but with several buying stations
+
+# Options to handle multiple matches (homonyms in abbreviated names and multiple buying stations)
 # - resolve manually
 # - apply distance threshold
 # - apply nearest match 
 # Currently we do nearest match. 
 
 coopbs_tomatch$COOP_BS_ID %>% unique() %>% length()
-surv_coop_bs$COOP_BS_ID %>% unique() %>% length()
-# names(surv_coop_bs)
-surv_coop_bs %>% 
-  select(COOP_ID, COOP_POINT_ID, COOP_BS_ID, COOP_SURVEY_ID, VILLAGE_SURVEY_ID, ssicooperative_name, ssicooperative_other, 
+vil_links_bs$COOP_BS_ID %>% unique() %>% length()
+
+# names(vil_links_bs)
+vil_links_bs %>% 
+  select(COOP_ID, COOP_POINT_ID, COOP_BS_ID, VILLAGE_SURVEY_ID,
          COOP_ABRV_NAME, 
          SUPPLIER_ABRVNAME, COOP_SIMPLIF_ABRV_NAME, SUPPLIER_FULLNAME, 
-         LONGITUDE, LATITUDE) %>% 
+         BS_LONGITUDE, BS_LATITUDE) %>% 
   View()
-  
 
-## Join coop survey with village survey ---------
-# ... to make links
+# Remove links with a coop but not matched to IC2B
+# i.e. keep only links with an IC2B buying station or another buyer than a coop. 
+nrow(vil_links)
+nrow(vil_links_bs)
+vil_links_bs = 
+  vil_links_bs %>% 
+  filter(!(!is.na(COOP_SIMPLIF_ABRV_NAME) & is.na(COOP_BS_ID)))
+nrow(vil_links_bs)
 
-vil_bs =
-  vil %>% 
-  inner_join(surv_coop_bs %>% select(VILLAGE_SURVEY_ID, COOP_SIMPLIF_ABRV_NAME, BS_LONGITUDE = LONGITUDE, BS_LATITUDE = LATITUDE, 
-                               COOP_BS_ID), # to be able to match back to IC2B. 
-            by = "VILLAGE_SURVEY_ID", # "COOP_SIMPLIF_ABRV_NAME" == "SIMPLIF_ABRVNAME"
-            multiple = "all") 
-
+# this removes 83 links and 15 villages 
+vil_links_bs$VILLAGE_SURVEY_ID %>% unique() %>% length()
+vil_links$VILLAGE_SURVEY_ID %>% unique() %>% length()
+vil$VILLAGE_SURVEY_ID %>% unique() %>% length()
 
 # Spatialize -----------
-sfvil_bs = 
-  vil_bs %>% 
+
+sfvil_links_bs = 
+  #vil_bs
+  vil_links_bs %>% 
   mutate(
     `_loc_location_x_y_longitude` = as.numeric(`_loc_location_x_y_longitude`),
     `_loc_location_x_y_latitude` = as.numeric(`_loc_location_x_y_latitude`),
@@ -195,76 +241,76 @@ sfvil_bs =
   st_as_sf(coords = c("PRO_LONGITUDE", "PRO_LATITUDE"), crs = 4326, remove = FALSE) %>% 
   st_transform(civ_crs) 
 
-nrow(vil_bs)
-nrow(sfvil_bs)
+nrow(vil_links_bs)
+nrow(sfvil_links_bs)
 
 ### Add departments
-sfvil_bs <- st_join(sfvil_bs,
-                   departements[,c("LVL_4_CODE", "LVL_4_NAME")],
-                   join = st_intersects) 
+sfvil_links_bs <- 
+  sfvil_links_bs %>% 
+  st_join(departements[,c("LVL_4_CODE", "LVL_4_NAME")],
+          join = st_intersects) 
 
 ## Make distance var ------------
 
 # start from the above, not the merger
-vil_sfbs = 
-  sfvil_bs %>% 
+vil_links_sfbs = 
+  sfvil_links_bs %>% 
   st_drop_geometry() %>% 
   # filter(!is.na(BS_LONGITUDE)) %>% 
   st_as_sf(coords = c("BS_LONGITUDE", "BS_LATITUDE"), crs = 4326, remove = FALSE) %>% 
   st_transform(civ_crs) 
 
-nrow(vil_sfbs)
+nrow(vil_links_sfbs)
 
 ggplot()+
-  geom_sf(data = sfvil_bs, col = "grey")  +
-  geom_sf(data = vil_sfbs, aes(col=COOP_SIMPLIF_ABRV_NAME))  +
+  geom_sf(data = sfvil_links_bs, col = "grey")  +
+  geom_sf(data = vil_links_sfbs, aes(col=COOP_SIMPLIF_ABRV_NAME))  +
   geom_sf(data = departements, fill = "transparent")
 
 ggplot()+
-  geom_sf(data = sfvil_bs, aes(col = COOP_SIMPLIF_ABRV_NAME))  +
-  geom_sf(data = vil_sfbs, col="black")  +
+  geom_sf(data = sfvil_links_bs, aes(col = COOP_SIMPLIF_ABRV_NAME))  +
+  geom_sf(data = vil_links_sfbs, col="black")  +
   geom_sf(data = departements, fill = "transparent")
 
-vil_sfbs$LINK_DISTANCE_METERS <- 
-  st_distance(sfvil_bs, vil_sfbs, by_element = TRUE)
+vil_links_sfbs$LINK_DISTANCE_METERS <- 
+  st_distance(sfvil_links_bs, vil_links_sfbs, by_element = TRUE)
 
-summary(vil_sfbs$LINK_DISTANCE_METERS)
+summary(vil_links_sfbs$LINK_DISTANCE_METERS)
 
-vil_bs = 
-  vil_sfbs %>% 
+vil_links_bs = 
+  vil_links_sfbs %>% 
   st_drop_geometry()
 
 vil$VILLAGE_SURVEY_ID %>% unique() %>% length() == nrow(vil)
-vil_bs$VILLAGE_SURVEY_ID %>% unique() %>% length() == nrow(vil_bs)
 
 # For a given village, keep only the closest buying station of the closest coop among the possibly several 
 # buying stations and possibly several coops matched based on the abbreviated name, 
-vil_bs_closestbs =
-  vil_bs %>% 
+vil_links_bs_closestbs =
+  vil_links_bs %>% 
   group_by(VILLAGE_SURVEY_ID) %>% 
   mutate(SMALLEST_DIST = min(LINK_DISTANCE_METERS)) %>% 
   ungroup() %>% 
   filter(LINK_DISTANCE_METERS == SMALLEST_DIST)
 
 # check that this goes back to one row per village 
-vil_bs_closestbs$VILLAGE_SURVEY_ID %>% unique() %>% length() 
-nrow(vil_bs_closestbs) 
-# vil_bs %>% 
+vil_links_bs_closestbs$VILLAGE_SURVEY_ID %>% unique() %>% length() 
+nrow(vil_links_bs_closestbs) 
+# vil_links_bs %>% 
 #   filter(VILLAGE_SURVEY_ID==15) %>% 
 #   distinct()
 # there is one remaining duplicate on everything, I don't understand why. I move on. 
-vil_bs_closestbs = 
-  vil_bs_closestbs %>% 
+vil_links_bs_closestbs = 
+  vil_links_bs_closestbs %>% 
   distinct(VILLAGE_SURVEY_ID, .keep_all = TRUE)
 
-vil_bs_closestbs$VILLAGE_SURVEY_ID %>% unique() %>% length() 
-nrow(vil_bs_closestbs) 
+vil_links_bs_closestbs$VILLAGE_SURVEY_ID %>% unique() %>% length() 
+nrow(vil_links_bs_closestbs) 
 
 
 # Export ----
 
 toexport = 
-  vil_bs_closestbs %>% 
+  vil_links_bs_closestbs %>% 
   mutate(YEAR = 2021, 
          DATA_SOURCE = "SUSTAINCOCOA",
          PRO_ID = paste0("SUSTAINCOCOA_VILLAGE_",VILLAGE_SURVEY_ID)) %>% 
